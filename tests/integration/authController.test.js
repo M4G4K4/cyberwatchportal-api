b82f21b6faf23e8d65aa653b22d8dc356559392a
Pedro Dias <pedro158a@gmail.com>
2021-08-15 23:10:48 +0100

    Add new test

--- tests/integration/authController.test.js
+++ tests/integration/authController.test.js
@@ -1,8 +1,8 @@
 const request = require('supertest');
 const app = require('../../app');
 
-describe('GET /auth', () => {
-  it('responds with json', async () => {
+describe('POST /auth', () => {
+  it('Register user', async () => {
     const userRegister = {
       first_name: "sdfsdf",
       last_name: "hgdjrsdg",
@@ -11,6 +11,16 @@ describe('GET /auth', () => {
       password: "password"
     }
     const res = await request(app).post('/auth/register').send(userRegister);
+
+    expect(res.statusCode).toEqual(201);
+  });
+
+  it('Login User ', async () => {
+    const userLogin = {
+      email: "pedrovsdias@gmail.com",
+      password: "password"
+    }
+    const res = await request(app).post('/auth/login').send(userLogin);
 
     expect(res.statusCode).toEqual(200);
   });
