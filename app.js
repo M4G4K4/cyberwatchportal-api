a39fe8bccbab042e2393faf0094718fb2bf63949
Pedro Dias <pedro158a@gmail.com>
2021-11-14 08:11:46 -0800

    Add package helmet to include secure http headers

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const helmet = require('helmet');
 const createError = require('http-errors');
 
 const app = express();
 
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
