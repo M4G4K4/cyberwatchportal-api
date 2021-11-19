cd2186d87210fb43e9c65a9121004fc7ca176310
Pedro Dias <pedro158a@gmail.com>
2021-11-19 18:47:11 +0000

    Implment rabbitmq sender

--- src/config/rabbitMqConnection.js
+++
@@ -1,5 +0,0 @@
-const rabbitMq = require('amqplib/callback_api').connect(process.env.RABBITMQ_CONNECTION);
-
-module.exports={
-    rabbitMq
-}
--- src/services/rabbitmqService.js
+++ src/services/rabbitmqService.js
@@ -1,2 +1,11 @@
-// See this
-// https://github.com/sergey-suslov/node-rabbitmq-example
+const rabbit = require('../config/rabbit');
+
+async function send(content) {
+    const broker = await rabbit.getInstance();
+    await broker.send(process.env.RABBITMQ_CHANNEL, Buffer.from(JSON.stringify(content)));
+    content = '';
+}
+
+module.exports = {
+    send
+}
